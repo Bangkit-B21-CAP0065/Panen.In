f9a18e43631e0dada0ce21dc87fb9ef395d3562b
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-05-31 16:59:29 +0700

    membuat bottom navbar (belum bisa switch fragment)

--- app/src/main/java/com/panenin/bangkit/b21/cap0065/MainActivity.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/MainActivity.kt
@@ -2,10 +2,27 @@ package com.panenin.bangkit.b21.cap0065
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.panenin.bangkit.b21.cap0065.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView : BottomNavigationView = binding.navView
+        val navController = findNavController(this, R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_shopping, R.id.navigation_profile
+        ).build()
+        setupActionBarWithNavController(this, navController, appBarConfiguration)
+        setupWithNavController(navView, navController)
     }
 }
