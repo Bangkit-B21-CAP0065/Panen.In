872b99b4cea6dad3f2bd7a99830350bddb2ce65a
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-01 00:52:06 +0700

    init page weather, recommendation, and visualization

---
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
@@ -0,0 +1,12 @@
+package com.panenin.bangkit.b21.cap0065.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.panenin.bangkit.b21.cap0065.R
+
+class RecommendationCropActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommendation_crop)
+    }
+}
