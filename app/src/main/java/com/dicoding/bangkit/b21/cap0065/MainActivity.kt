c4308f97d0b8bb9603f063e015de400951100766
Mutia Rahmatun Husna <mutia.rahmatun@ui.ac.id>
2021-05-31 08:59:05 +0700

    init project

---
+++ app/src/main/java/com/dicoding/bangkit/b21/cap0065/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.bangkit.b21.cap0065
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
