dd5fe59f11182b0831c02843443e1fa6ce7f6fed
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-01 20:38:33 +0700

    dropdown dummy (done)

--- app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
@@ -3,33 +3,39 @@ package com.panenin.bangkit.b21.cap0065.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import android.widget.Toast
 import com.panenin.bangkit.b21.cap0065.R
 import com.panenin.bangkit.b21.cap0065.databinding.ActivityRecommendationCropBinding
 
-class RecommendationCropActivity : AppCompatActivity() {
+class RecommendationCropActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityRecommendationCropBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        // Create an ArrayAdapter
-        val adapter = ArrayAdapter.createFromResource(this,
-            R.array.city_list, android.R.layout.simple_spinner_item)
-        // Specify the layout to use when the list of choices appears
+        textView = binding.textView
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.city_list,
+            android.R.layout.simple_spinner_item
+        )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        // Apply the adapter to the spinner
         binding.spinner.adapter = adapter
-
+        binding.spinner.onItemSelectedListener = this
     }
-
-    fun getValues(view: View) {
-        val binding = ActivityRecommendationCropBinding.inflate(layoutInflater)
-
-        Toast.makeText(this, "Spinner 1 ${binding.spinner.selectedItem} " +
-                "\nSpinner 2 ${binding.spinner2.selectedItem}", Toast.LENGTH_LONG).show()
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val text: String = parent?.getItemAtPosition(position).toString()
+        textView.text = text
+        Toast.makeText(this, "Spinner 1 choosed $text", Toast.LENGTH_LONG).show()
     }
 
 }
