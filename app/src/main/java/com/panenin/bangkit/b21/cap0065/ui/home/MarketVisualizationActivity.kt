bca2078cae4361b3ab33efbed332ae6ea163ca35
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-01 20:20:05 +0700

    weather predict front end (done)

--- app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/MarketVisualizationActivity.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/MarketVisualizationActivity.kt
@@ -2,11 +2,16 @@ package com.panenin.bangkit.b21.cap0065.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.panenin.bangkit.b21.cap0065.R
+import com.panenin.bangkit.b21.cap0065.databinding.ActivityMarketVisualizationBinding
 
 class MarketVisualizationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_market_visualization)
+        val binding = ActivityMarketVisualizationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
@@ -2,11 +2,34 @@ package com.panenin.bangkit.b21.cap0065.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.panenin.bangkit.b21.cap0065.R
+import com.panenin.bangkit.b21.cap0065.databinding.ActivityRecommendationCropBinding
 
 class RecommendationCropActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recommendation_crop)
+        val binding = ActivityRecommendationCropBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Create an ArrayAdapter
+        val adapter = ArrayAdapter.createFromResource(this,
+            R.array.city_list, android.R.layout.simple_spinner_item)
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Apply the adapter to the spinner
+        binding.spinner.adapter = adapter
+
     }
+
+    fun getValues(view: View) {
+        val binding = ActivityRecommendationCropBinding.inflate(layoutInflater)
+
+        Toast.makeText(this, "Spinner 1 ${binding.spinner.selectedItem} " +
+                "\nSpinner 2 ${binding.spinner2.selectedItem}", Toast.LENGTH_LONG).show()
+    }
+
 }
