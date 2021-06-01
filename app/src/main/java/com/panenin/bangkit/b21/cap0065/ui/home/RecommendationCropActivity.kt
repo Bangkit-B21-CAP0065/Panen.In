daff663e41f4054c85b4e5a9a38c4364fc0110a1
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-01 22:01:25 +0700

    prepare when click button predict now

--- app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
@@ -1,37 +1,83 @@
 package com.panenin.bangkit.b21.cap0065.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.panenin.bangkit.b21.cap0065.R
 import com.panenin.bangkit.b21.cap0065.databinding.ActivityRecommendationCropBinding
 
-class RecommendationCropActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+class RecommendationCropActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
+
+    private lateinit var chosenRegion : String
+    private lateinit var chosenPlantType : String
+    private lateinit var chosenDuration : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityRecommendationCropBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val adapter = ArrayAdapter.createFromResource(
+        val adapterRegionChoosed = ArrayAdapter.createFromResource(
             this,
-            R.array.city_list,
+            R.array.region_list,
             android.R.layout.simple_spinner_item
         )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinner.adapter = adapter
-        binding.spinner.onItemSelectedListener = this
+        adapterRegionChoosed.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val adapterPlantTypeChoosed = ArrayAdapter.createFromResource(
+            this,
+            R.array.plant_type_list,
+            android.R.layout.simple_spinner_item
+        )
+        adapterPlantTypeChoosed.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val adapterDurationChoosed = ArrayAdapter.createFromResource(
+            this,
+            R.array.duration_list,
+            android.R.layout.simple_spinner_item
+        )
+        adapterDurationChoosed.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.regionDropdown.adapter = adapterRegionChoosed
+        binding.regionDropdown.onItemSelectedListener = this
+
+        binding.plantTypeDropdown.adapter = adapterPlantTypeChoosed
+        binding.plantTypeDropdown.onItemSelectedListener = this
+
+        binding.durationDropdown.adapter = adapterDurationChoosed
+        binding.durationDropdown.onItemSelectedListener = this
+
+        chosenRegion = resources.getStringArray(R.array.region_list).first()
+        chosenPlantType = resources.getStringArray(R.array.plant_type_list).first()
+        chosenDuration =  resources.getStringArray(R.array.duration_list).first()
+
+        binding.predictWeatherButton.setOnClickListener{
+            Toast.makeText(this,
+                "region: $chosenRegion, tipe: $chosenPlantType, durasi: $chosenDuration", Toast.LENGTH_SHORT).show()
+        }
     }
     override fun onNothingSelected(parent: AdapterView<*>?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val text: String = parent?.getItemAtPosition(position).toString()
-        Toast.makeText(this, "Region choosed: $text", Toast.LENGTH_LONG).show()
+        when (parent?.id) {
+            R.id.region_dropdown -> {
+                Toast.makeText(this, "position $position, Region Selected: " + parent.selectedItem.toString(), Toast.LENGTH_SHORT).show()
+                chosenRegion = parent.selectedItem.toString()
+            }
+            R.id.plant_type_dropdown -> {
+                Toast.makeText(this, "position $position, Plan type Selected: " + parent.selectedItem.toString(), Toast.LENGTH_SHORT).show()
+                chosenPlantType = parent.selectedItem.toString()
+            }
+            R.id.duration_dropdown -> {
+                Toast.makeText(this, "position $position, Duration Selected: " + parent.selectedItem.toString(), Toast.LENGTH_SHORT).show()
+                chosenDuration = parent.selectedItem.toString()
+            }
+        }
     }
 
 }
