9597d4a79ff924d19e2ccc13a4dbd717513cd398
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-03 00:48:14 +0700

    bar chart dummy in recommendation crop

--- app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/RecommendationCropActivity.kt
@@ -7,6 +7,10 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import com.panenin.bangkit.b21.cap0065.R
 import com.panenin.bangkit.b21.cap0065.databinding.ActivityRecommendationCropBinding
 
@@ -16,10 +20,11 @@ class RecommendationCropActivity : AppCompatActivity(), AdapterView.OnItemSelect
     private lateinit var chosenRegion : String
     private lateinit var chosenPlantType : String
     private lateinit var chosenDuration : String
+    private lateinit var binding: ActivityRecommendationCropBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityRecommendationCropBinding.inflate(layoutInflater)
+        binding = ActivityRecommendationCropBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val adapterRegionChoosed = ArrayAdapter.createFromResource(
@@ -57,6 +62,8 @@ class RecommendationCropActivity : AppCompatActivity(), AdapterView.OnItemSelect
         chosenDuration =  resources.getStringArray(R.array.duration_list).first()
 
         binding.predictWeatherButton.setOnClickListener{
+            var tempDurationNumber = chosenDuration.toInt()
+            setBarChart(tempDurationNumber)
             Toast.makeText(this,
                 "region: $chosenRegion, tipe: $chosenPlantType, durasi: $chosenDuration", Toast.LENGTH_SHORT).show()
         }
@@ -64,9 +71,41 @@ class RecommendationCropActivity : AppCompatActivity(), AdapterView.OnItemSelect
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = getString(R.string.title_reccomendation_page)
     }
+
+    private fun setBarChart(numberOfBar : Int) {
+        val entries = ArrayList<BarEntry>()
+//        entries.add(BarEntry(8f, 0))
+//        entries.add(BarEntry(2f, 1))
+//        entries.add(BarEntry(5f, 2))
+//        entries.add(BarEntry(20f, 3))
+//        entries.add(BarEntry(15f, 4))
+//        entries.add(BarEntry(19f, 5))
+
+        val barDataSet = BarDataSet(entries, "Cells")
+
+        val labels = ArrayList<String>()
+        for (i in 1..numberOfBar) {
+            entries.add(BarEntry(8f, i-1))
+            labels.add("Round $i")
+        }
+//        labels.add("18-Jan")
+//        labels.add("19-Jan")
+//        labels.add("20-Jan")
+//        labels.add("21-Jan")
+//        labels.add("22-Jan")
+//        labels.add("23-Jan")
+        val data = BarData(labels, barDataSet)
+        binding.barChart.data = data // set the data and list of lables into chart
+
+        barDataSet.color = ResourcesCompat.getColor(getResources(), R.color.yellow_500, null);
+
+        binding.barChart.animateY(5000)
+    }
+
     override fun onNothingSelected(parent: AdapterView<*>?) {
         TODO("not implemented")
     }
+
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         when (parent?.id) {
             R.id.region_dropdown -> {
