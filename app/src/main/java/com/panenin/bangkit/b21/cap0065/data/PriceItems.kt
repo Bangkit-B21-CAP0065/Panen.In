50ddc33c43b559600fdd117d703e2605f67ba50a
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-05 12:34:51 +0700

    prices can not be null

--- app/src/main/java/com/panenin/bangkit/b21/cap0065/data/PriceItems.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/data/PriceItems.kt
@@ -7,7 +7,7 @@ import kotlinx.android.parcel.Parcelize
 data class PriceItems (
     var year: String? = "0000",
     var month: Int? = 0,
-    var averagePrice: String? = "0",
-    var minPrice: String? = "0",
-    var maxPrice: String? = "0"
+    var averagePrice: String = "0",
+    var minPrice: String = "0",
+    var maxPrice: String = "0"
 ): Parcelable
