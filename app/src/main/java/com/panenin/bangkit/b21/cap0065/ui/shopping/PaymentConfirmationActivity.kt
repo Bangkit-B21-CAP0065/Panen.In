58177c7792e2aae50ba67c2039307b3c0edecb08
Mutia Rahmatun Husna <mutia.rahmatun@ui.ac.id>
2021-06-09 23:54:49 +0700

    implement checkout and payment confirmation

---
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/shopping/PaymentConfirmationActivity.kt
@@ -0,0 +1,12 @@
+package com.panenin.bangkit.b21.cap0065.ui.shopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.panenin.bangkit.b21.cap0065.R
+
+class PaymentConfirmationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_confirmation)
+    }
+}
