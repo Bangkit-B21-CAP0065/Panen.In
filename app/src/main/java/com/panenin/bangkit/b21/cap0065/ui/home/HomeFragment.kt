3ad461626156792d3126ad83397c4f967120628d
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-06-09 20:40:36 +0700

    delete toast

--- app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/HomeFragment.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/home/HomeFragment.kt
@@ -29,17 +29,14 @@ class HomeFragment : Fragment(){
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         binding.buttonPredictWeather.setOnClickListener{
-            Toast.makeText(requireContext(), "button predict clicked!", Toast.LENGTH_SHORT).show()
             val intent = Intent(activity, WeatherPredictActivity::class.java)
             startActivity(intent)
         }
         binding.buttonRecommendCrop.setOnClickListener{
-            Toast.makeText(requireContext(), "button recommend clicked!", Toast.LENGTH_SHORT).show()
             val intent = Intent(activity, CropRecommendationActivity::class.java)
             startActivity(intent)
         }
         binding.buttonMarketVisualization.setOnClickListener{
-            Toast.makeText(requireContext(), "button market visualization works!", Toast.LENGTH_SHORT).show()
             val intent = Intent(activity, MarketVisualizationActivity::class.java)
             startActivity(intent)
         }
