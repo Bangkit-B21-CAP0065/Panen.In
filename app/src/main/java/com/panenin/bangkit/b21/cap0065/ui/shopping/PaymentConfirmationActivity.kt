a8863fc12c9600321397ce21e9d77387ecaf0be8
Mutia Rahmatun Husna <mutia.rahmatun@ui.ac.id>
2021-06-09 21:40:01 +0700

    implement success page

--- app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/shopping/PaymentConfirmationActivity.kt
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/shopping/PaymentConfirmationActivity.kt
@@ -1,12 +1,23 @@
 package com.panenin.bangkit.b21.cap0065.ui.shopping
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.panenin.bangkit.b21.cap0065.R
+import com.panenin.bangkit.b21.cap0065.databinding.ActivityPaymentConfirmationBinding
 
 class PaymentConfirmationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPaymentConfirmationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_payment_confirmation)
+        binding = ActivityPaymentConfirmationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonConfirm.setOnClickListener{
+            val intent = Intent(this, SuccessActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/panenin/bangkit/b21/cap0065/ui/shopping/SuccessActivity.kt
@@ -0,0 +1,12 @@
+package com.panenin.bangkit.b21.cap0065.ui.shopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.panenin.bangkit.b21.cap0065.R
+
+class SuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success)
+    }
+}
